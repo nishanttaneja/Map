5972561b3fbea6d7d1d472b3bb8648fc8da5a31a
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-03-06 11:02:47 +0530

    add Map and InfoAndCurrentLocationStackView

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    // IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var infoAndCurrentLocationStackView: UIStackView!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var currentLocationButton: UIButton!
+    
+    // Variables | Constants
+    
+    
+    // IBActions
+    @IBAction func infoButtonAction(_ sender: UIButton) {}
+    
+    @IBAction func currentLocationButtonAction(_ sender: UIButton) {}
 }
 
+//MARK:- View Lifecycle
+extension ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        infoAndCurrentLocationStackView.configureUI()
+    }
+}
+
+//MARK:- UIStackView
+fileprivate extension UIStackView {
+    /// Configures basic UI. Includes adding Shadow and CornerRadius.
+    func configureUI() {
+        layer.cornerRadius = frame.height/8
+        layer.shadowRadius = 1
+        layer.shadowColor = UIColor.systemGray4.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = .zero
+    }
+}
