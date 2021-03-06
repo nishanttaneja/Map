d328772bcfe22cb34a1c4369137aab040f822ddf
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-03-06 11:29:07 +0530

    add current location access

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -5,23 +5,72 @@
 //  Created by Nishant Taneja on 06/03/21.
 //
 
-import UIKit
 import MapKit
 
 class ViewController: UIViewController {
     // IBOutlets
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var infoAndCurrentLocationStackView: UIStackView!
-    @IBOutlet weak var infoButton: UIButton!
-    @IBOutlet weak var currentLocationButton: UIButton!
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var infoAndCurrentLocationStackView: UIStackView!
+    @IBOutlet private weak var infoButton: UIButton!
+    @IBOutlet private weak var currentLocationButton: UIButton!
     
     // Variables | Constants
-    
+    private var locationManager = CLLocationManager()
+    private var hasLocationAccess: Bool {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways: return true
+        default:
+            locationManager.requestWhenInUseAuthorization()
+            return false
+        }
+    }
+    private var requestCurrentLocation: Bool = false {
+        willSet {
+            guard hasLocationAccess else {
+                print("location access is not authorised")
+                return
+            }
+            guard newValue else {
+                // Cancel request for current location
+                locationManager.stopUpdatingLocation()
+                currentLocationButton.setImage(UIImage(systemName: "location"), for: .normal)
+                return
+            }
+            // Request Current Location
+            locationManager.startUpdatingLocation()
+            currentLocationButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        }
+    }
+    private var currentLocation: CLLocation?
     
     // IBActions
-    @IBAction func infoButtonAction(_ sender: UIButton) {}
+    @IBAction private func infoButtonAction(_ sender: UIButton) {}
     
-    @IBAction func currentLocationButtonAction(_ sender: UIButton) {}
+    @IBAction private func currentLocationButtonAction(_ sender: UIButton) {
+        requestCurrentLocation = !requestCurrentLocation
+    }
+}
+
+//MARK:- CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let lastLocation = locations.last else { return }
+        if currentLocation != nil {
+            if lastLocation.coordinate.latitude != currentLocation!.coordinate.latitude,
+               lastLocation.coordinate.longitude != currentLocation!.coordinate.longitude {
+                currentLocation = lastLocation
+            }
+        } else {
+            currentLocation = lastLocation
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse: requestCurrentLocation = true
+        default: requestCurrentLocation = false
+        }
+    }
 }
 
 //MARK:- View Lifecycle
@@ -29,6 +78,12 @@ extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         infoAndCurrentLocationStackView.configureUI()
+        locationManager.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        requestCurrentLocation = true
     }
 }
 
